618af5a7e93698e133b6f34bd4a4110cb4f45265
jlvivero <josluivivgar@gmail.com>
2015-05-12 10:17:40 -0700

    First attempt by doing an overload of the methods, disadvantage is that the variable has to be initialized before the method can be used

---
+++ Test.java
@@ -0,0 +1,12 @@
+public class Test
+{
+	public static void main(String [] args)
+	{
+		int x = 0;
+		x = C.in("dame un numero",x);
+		System.out.println(x);
+		double y = 0.0;
+		y = C.in("dame un doble", y);
+		System.out.println(y);
+	}
+} 
